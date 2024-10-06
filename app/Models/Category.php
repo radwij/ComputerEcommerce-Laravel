65bd873ee88337f34c90d0ba0198b08818e143d1
radwij <118460863+radwij@users.noreply.github.com>
2024-10-07 02:16:23 +0700

    added manage category feature

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    // Everything but id is inputted by user and allowed to be sent to database
+    protected $guarded = [
+        'id',
+    ];
 }
